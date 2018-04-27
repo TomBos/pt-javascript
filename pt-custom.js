8fa35097ac72e5f4f65cfc280e789d637a9fc286
TomBos <tombosgaard@gmail.com>
2018-04-27 18:18:26 +0200

    Made currency configurable

--- pt-custom.js
+++ pt-custom.js
@@ -1,4 +1,6 @@
 //Graphing-Tracker.js
+$currency = '€';
+
 (function(){
 	var util = {};
 
@@ -182,13 +184,7 @@
 					ctx.lineTo( (index/totalRun * size.width) , (size.height - (( this.stats.data[i] - range.min ) / range.size * size.height )));
 				}
 				index++;
-			} /*else if( this.stats.data[i] == '' ) {
-				if( !first ) {
-					ctx.stroke();
-					first = true;
-				}
-				index++;
-			}*/
+			}
 		}
 		ctx.stroke();
 		var res = 'url(' + util.canvas.toDataURL() + ')';
@@ -456,18 +452,17 @@
 //Estimated USD difference for Profit Trailer 2.0
 //Created by ProfitTanker#0156 - If you need any help send me a DM.
 function estimate() {
-	
 	//Pairs
 	if ($('#dtPairsLogs thead').length > 0) {
 		$('#dtPairsLogs tbody tr').each(function() {
 			$(this).find('b').remove();
-			var difference = ($(this).find('td.bought-cost:last .current-value').text().replace("$", "").replace(",", "") - $(this).find('td.bought-cost:last .bought-cost').text().replace("$", "").replace(",", "")).toFixed(2);
-			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace("$", "").replace(",", "") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace("$", "").replace(",", "")).toFixed(8);
+			var difference = ($(this).find('td.bought-cost:last .current-value').text().replace($currency, "").replace(",", "") - $(this).find('td.bought-cost:last .bought-cost').text().replace($currency, "").replace(",", "")).toFixed(2);
+			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace($currency, "").replace(",", "") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace($currency, "").replace(",", "")).toFixed(8);
 			if (difference2 > 0) {
-				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>$' + difference.toLocaleString() + '</b>');
+				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + $currency + difference.toLocaleString() + '</b>');
 				$(this).find('td.current-value.blue-color:first .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			} else {
-				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>$' + difference.toLocaleString() + '</b>');
+				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>'+ $currency + difference.toLocaleString() + '</b>');
 				$(this).find('td.current-value.blue-color:first .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			}
 		});
@@ -499,13 +494,13 @@ function estimate() {
 	if ($('#dtDcaLogs thead').length > 0) {
 		$('#dtDcaLogs tbody tr').each(function() {
 			$(this).find('b').remove();
-			var difference = ($(this).find('td.current-value:last .current-value').text().replace("$", "") - $(this).find('td.current-value:last .bought-cost').text().replace("$", "")).toFixed(2);
-			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace("$", "") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace("$", "")).toFixed(8);
+			var difference = ($(this).find('td.current-value:last .current-value').text().replace($currency, "") - $(this).find('td.current-value:last .bought-cost').text().replace($currency, "")).toFixed(2);
+			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace($currency, "") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace($currency, "")).toFixed(8);
 			if (difference2 > 0) {
-				$(this).find('td.current-value:last').append('<b style="color:#05b16f;border-top: 1px solid;"><br>$' + difference + '</b>');
+				$(this).find('td.current-value:last').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + $currency + difference + '</b>');
 				$(this).find('td.current-value.blue-color:first').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			} else {
-				$(this).find('td.current-value:last').append('<b style="color:#d85353;border-top: 1px solid;"><br>$' + difference + '</b>');
+				$(this).find('td.current-value:last').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + $currency + difference + '</b>');
 				$(this).find('td.current-value.blue-color:first').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			}
 		});
@@ -539,13 +534,13 @@ function estimate() {
 	if ($('#dtDustLogs thead').length > 0) {
 		$('#dtDustLogs tbody tr').each(function() {
 			$(this).find('b').remove();
-			var difference = ($(this).find('td.bought-cost .current-value').text().replace("$", "") - $(this).find('td.bought-cost .bought-cost').text().replace("$", "")).toFixed(2);
-			var difference2 = ($(this).find('td.blue-color.current-value:first .current-value').text().replace("$", "") - $(this).find('td.blue-color.current-value:first .bought-cost').text().replace("$", "")).toFixed(8);
+			var difference = ($(this).find('td.bought-cost .current-value').text().replace("$", "") - $(this).find('td.bought-cost .bought-cost').text().replace($currency, "")).toFixed(2);
+			var difference2 = ($(this).find('td.blue-color.current-value:first .current-value').text().replace("$", "") - $(this).find('td.blue-color.current-value:first .bought-cost').text().replace($currency, "")).toFixed(8);
 			if (difference2 > 0) {
-				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>$' + difference + '</b>');
+				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + $currency + difference + '</b>');
 				$(this).find('td.blue-color.current-value:first').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			} else {
-				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>$' + difference + '</b>');
+				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + $currency + difference + '</b>');
 				$(this).find('td.blue-color.current-value:first').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
 			}
 		});
